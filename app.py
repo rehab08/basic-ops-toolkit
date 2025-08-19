629a4e254a302bc6c39b1faa0207196156e68622
rehab08 <jamesekakiti@gmail.com>
2025-08-19 10:27:30 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.write("Simple Calculator")
+
+# --- simple calculations ---
+st.header("Calculator")
+
+num1 = st.number_input("Enter first Number", value=0.0)
+num2 = st.number_input("Enter second Number", value=0.0)
+operation = st.selectbox("Choose operation", ["Add", "Subtract", "Multiply", "Divide"])
+
+if st.button("Submit"):
+    if operation == "Add":
+        result = num1 + num2
+    elif operation == "Subtract":
+        result = num1 - num2
+    elif operation == "Multiply":
+        result = num1 * num2
+    elif operation == "Divide":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Cannot divide by zero"
+    st.success(f"Result: {result}")
+
+import numpy
+import sympy
